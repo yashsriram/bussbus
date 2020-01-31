6eb1aea7367375d8af1e5a5ac230e7fb2905d133
Yashasvi Sriram <yash.3997@gmail.com>
2020-01-31 14:57:07 -0600

    adds pull to refresh layout

--- app/src/main/java/com/yashasvisriram/bussbus/MainActivity.kt
+++ app/src/main/java/com/yashasvisriram/bussbus/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.yashasvisriram.bussbus
 
 import android.os.Bundle
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -21,12 +21,16 @@ class MainActivity : AppCompatActivity() {
     private val baseUrl = "https://svc.metrotransit.org/"
     private val compositeDisposable = CompositeDisposable()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        // UI setup
+        stopDepartures1.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
+        // REST service setup
         val retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
@@ -34,13 +38,24 @@ class MainActivity : AppCompatActivity() {
             .build()
         val apiService = retrofit.create(ApiService::class.java)
 
+        // Initial REST call
+        restCall(apiService)
+
+        // Swipe refresh setup
+        swipeContainer.setOnRefreshListener {
+            restCall(apiService)
+        }
+    }
+
+    private fun restCall(apiService: ApiService) {
         val stopDepartures = apiService.getStopDepartures("16154")
         stopDepartures!!
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(object : SingleObserver<List<StopDeparture>?> {
                 override fun onSuccess(stopDepartures: List<StopDeparture>) {
-                    recyclerView.adapter = StopDeparturesAdapter(stopDepartures)
+                    stopDepartures1.adapter = StopDeparturesAdapter(stopDepartures)
+                    swipeContainer.isRefreshing = false
                 }
 
                 override fun onSubscribe(d: Disposable) {
@@ -49,6 +64,7 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onError(e: Throwable) {
                     Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_LONG).show()
+                    swipeContainer.isRefreshing = false
                 }
             })
     }
