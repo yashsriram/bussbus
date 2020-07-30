33181d4a75c7a2f928a684348f57eff15cf43401
Yashasvi Sriram <yash.3997@gmail.com>
2020-07-30 15:07:10 -0500

    moves StopDeparturesAdapter -> StopDeparturesActivity.kt and makes it private, deletes StopDeparturesAdapter.kt

--- app/src/main/java/com/yashasvisriram/bussbus/StopDeparturesActivity.kt
+++ app/src/main/java/com/yashasvisriram/bussbus/StopDeparturesActivity.kt
@@ -1,9 +1,12 @@
 package com.yashasvisriram.bussbus
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -162,5 +165,48 @@ class StopDeparturesActivity : AppCompatActivity() {
     private fun updateTimeSinceLastSync() {
         val timeSinceLastSyncInMillis = System.currentTimeMillis() - lastSyncTimestamp
         activityStopDepartures.sync.text = "${timeSinceLastSyncInMillis / 1000 / 60} min ago"
+    }
+}
+
+private class StopDeparturesAdapter(
+    private val departures: List<StopDeparture>,
+    private val context: Context
+) :
+    RecyclerView.Adapter<StopDeparturesAdapter.Holder>() {
+
+    private class Holder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        var view: View
+        var due: TextView
+        var routeAndTerminal: TextView
+
+        init {
+            v.setOnClickListener(this)
+            view = v
+            due = v.findViewById(R.id.due)
+            routeAndTerminal = v.findViewById(R.id.routeAndTerminal)
+        }
+
+        override fun onClick(v: View) {
+        }
+    }
+
+    override fun getItemCount() = departures.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val inflatedView =
+            LayoutInflater.from(context).inflate(R.layout.stop_departure, parent, false)
+        return Holder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val departure = departures[position]
+        holder.view.background = when (departure.actual) {
+            true -> ContextCompat.getDrawable(context, R.drawable.live_departure)
+            false -> ContextCompat.getDrawable(context, R.drawable.normal_departure)
+        }
+        holder.view.setPadding(20, 20, 20, 20)
+        holder.due.text = " ${departure.departureText!!}".padOrTruncateString(7)
+        holder.routeAndTerminal.text =
+            "${departure.route}${departure.terminal} ".padOrTruncateString(3)
     }
 }
