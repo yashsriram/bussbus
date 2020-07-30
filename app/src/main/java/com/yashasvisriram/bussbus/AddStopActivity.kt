ef8c36194b8d4ac126ee9f80fd547b6152ebbda6
Yashasvi Sriram <yash.3997@gmail.com>
2020-07-30 16:15:03 -0500

    adds title, stop id, stop nickname and add btn views to AddStopActivity

--- app/src/main/java/com/yashasvisriram/bussbus/AddStopActivity.kt
+++ app/src/main/java/com/yashasvisriram/bussbus/AddStopActivity.kt
@@ -7,5 +7,6 @@ class AddStopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_stop)
+        title = "Add a stop"
     }
 }
